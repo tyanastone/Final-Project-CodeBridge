02c6066863dbaa5b6b1f43052b6a0c00041621dc
tyanastone <42702778+tyanastone@users.noreply.github.com>
2018-09-18 15:35:30 -0400

    added and adjusted content

--- Final_Project/app.js
+++ Final_Project/app.js
@@ -1,9 +1,34 @@
+// created function to open nav bar
 function openNav() {
     document.getElementById('nav').style.width = "100%";
 }
+// created function to close nav bar
 function closeNav() {
     document.getElementById('nav').style.width = "0%";
 }
+// created an alert box so that submit button goes somewhere
+function contactForm() {
+    alert("Thanks for submitting");
+}
+
+// aligned content to center of div on bragging rights page and increased font size for instructions and title
+document.getElementById("allCereal").style.textAlign = "center";
+document.getElementById("haul2").style.textAlign = "center";
+document.getElementById("haul3").style.textAlign = "center";
+document.getElementById("haul4").style.textAlign = "center";
+document.getElementById("form2").style.textAlign = "center";
+document.getElementById("titleBrags").style.textAlign = "center";
+document.getElementById("titleBrags").style.fontSize = "60px";
+document.getElementById("bragsInstructions").style.textAlign = "center";
+document.getElementById("bragsInstructions").style.fontSize = "20px";
+
+
+
+
+
+
+
+
 
 
 
