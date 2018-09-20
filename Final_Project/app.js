b5c71f056ef295a21e4dbb838f6783cfc9ca6d01
tyanastone <42702778+tyanastone@users.noreply.github.com>
2018-09-19 21:34:29 -0400

    finished project

--- Final_Project/app.js
+++ Final_Project/app.js
@@ -9,29 +9,7 @@ function closeNav() {
     document.getElementById('nav').style.width = "0%";
 }
 
-
-
-
 // find an element with the ID of button, listen for the click event and run the function, used query selector to select the class modal-bg
-
-// created an alert box so that submit button goes somewhere
-// function contactForm() {
-//     alert("Thanks for submitting");
-// }
-
-
-
-
-
-
-// function validateForm() {
-//     var x = document.forms["myForm"]["fname"].value;
-//     if (x == "") {
-//         alert("Name must be filled out");
-//         return false;
-//     }
-// }
-
 document.getElementById('button').addEventListener('click', function() {
     document.querySelector('.bg-modal').style.display = 'flex';
 });
@@ -47,6 +25,7 @@ function validate() {
     var Email = document.getElementById("Email").value;
     var message = document.getElementById("message").value;
     var pattern = document.getElementById("pattern").value;
+    // used "^[0-9]" to make sure that it checks for numbers 0-9, regexp matches text with a pattern
     var patcheck = new RegExp("^[0-9]");
 //    created if else statements
     if(uname==="")
@@ -57,6 +36,8 @@ function validate() {
     }
     else
     {
+        // definition help from:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+        // The indexOf() method returns the index within the calling String object of the first occurrence of the specified value, starting the search at fromIndex. Returns -1 if the value is not found.
         document.getElementById("div1").innerHTML="";
     }
     if(Email.indexOf("@")> -1)
@@ -78,7 +59,7 @@ function validate() {
     {
         document.getElementById("div3").innerHTML="";
     }
-    
+    // .test, tests for a string, in this cases tests for a pattern
     if(!patcheck.test(pattern))
     {
         document.getElementById("div4").innerHTML="Only Numbers";
@@ -90,23 +71,7 @@ function validate() {
     }
 }
 
-// aligned content to center of div on bragging rights page and increased font size for instructions and title
-// document.getElementById("allCereal").style.textAlign = "center";
-// document.getElementById("haul2").style.textAlign = "center";
-// document.getElementById("haul3").style.textAlign = "center";
-// document.getElementById("haul4").style.textAlign = "center";
-// document.getElementById("haul6").style.textAlign = "center";
-// document.getElementById("haul7").style.textAlign = "center";
-// document.getElementById("form2").style.textAlign = "center";
-// document.getElementById("titleBrags").style.textAlign = "center";
-// document.getElementById("titleBrags").style.fontSize = "60px";
-// document.getElementById("bragsInstructions").style.textAlign = "center";
-// document.getElementById("bragsInstructions").style.fontSize = "20px";
-// document.getElementById("footer").style.textAlign = "center";
-
-
-
-
+// aligned about me page and description of store policies
 document.getElementById("myPicture2").style.textAlign = "center";
 document.getElementById("aboutTitle").style.textAlign = "center";
 document.getElementById("policyDescription").style.textAlign = "center";
